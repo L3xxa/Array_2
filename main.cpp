5be819b6af1d0d2c8e9a3784fac2a2f0d85e4cb6
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-04 15:07:01 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
+#include <iomanip>
 #include <iostream>
 using namespace std;
 
 int main() {
 
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
 
+    const int size_x = 5;
+    const int size_y = 5;
+    int arr[size_x][size_y];
+
+    int current_value = x;
+
+    for (int i = 0; i < size_x; i++) {
+        for (int j = 0; j < size_y; j++) {
+            arr[i][j] = current_value;
+            current_value *= 2;
+            cout << setw(10) << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 
 
     return 0;
